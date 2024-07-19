78cae38f77deee0b368d340cdb36dfb5e4c2e861
Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
2024-07-19 12:07:17 -0400

    Add missing `:`

--- R/lovecraft.R
+++ R/lovecraft.R
@@ -1,12 +1,12 @@
 #' @title Lovecraft's Corpus
 #'
 #' @description
-#' See \code{\link[lovecraftr:lovecraft]{lovecraftr:lovecraft}} for documentation.
+#' See \code{\link[lovecraftr::lovecraft]{lovecraftr::lovecraft}} for documentation.
 #'
 #' @return A tibble of Lovecraft's corpus.
 #'
 #' @export
-#' @seealso \code{\link[lovecraftr:lovecraft]{lovecraftr:lovecraft}}
+#' @seealso \code{\link[lovecraftr::lovecraft]{lovecraftr::lovecraft}}
 #' @usage lovecraft_corpus()
 lovecraft_corpus <- function() {
   lovecraftr::lovecraft
